4f14c494e4574589c89575a691ce4e0465184bee
bekdev01 <bekzod.m070@gmail.com>
2022-06-05 20:29:17 +0500

    sd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import {Route} from "react-router-dom";
+import {Routes,Route} from "react-router";
+import SignIn from "./Pages/SignIn";
+import Dashboard from "./Pages/Dashboard";
+import Main from "./Pages/Main";
 
 
 function App() {
     return (
         <div>
-
+            <Routes>
+                <Route path={"/login"}  element={<SignIn/>}/>
+                <Route path={"/dashboard"}  element={<Dashboard/>}/>
+                <Route path={"/"}  element={<Main/>}/>
+            </Routes>
         </div>
     );
 }
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,3 +1,11 @@
+import Header from "../components/Header";
+import MainPage from "./MainPage";
+import News from "./News";
+import Writers from "./Writer/writer";
+import Books from "./Books";
+import Picture from "./PicturePage";
+import Footer from "../components/Footer";
+
 function Main() {
 
     return (
