157c1c6a29f9497783ebfa2e50d5af0ab97b9f29
Temur01gul <suvonovtemur2008@gmail.com>
2022-06-06 18:25:16 +0500

    sd

--- src/Pages/VideoPage/video.js
+++ src/Pages/VideoPage/video.js
@@ -6,7 +6,6 @@ function Video() {
         <Box sx={{px:10,py:12}}>
             <video width="100%" height="540" controls>
                 <source src="https://youtu.be/O5Wdy0vUyLs"/>
-                    {/*<source src="https://www.youtube.com/watch?v=O5Wdy0vUyLs" type="video/ogg"/>*/}
 
             </video>
 
